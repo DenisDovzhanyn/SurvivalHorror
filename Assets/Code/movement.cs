a2141ed11d4c5978f95e9c1aa4142b877dbdc8b7
Denis <dovzhanyndenis03@gmail.com>
2025-07-19 10:54:43 -0400

    fixed #8 - fix: while moving between camera positions, holding down a movement key between the positions will not change the direction the player is moving in, this makes the movement feel a bit smoother between the transitions

--- Assets/Code/movement.cs
+++ Assets/Code/movement.cs
@@ -5,6 +5,8 @@ public class movement : MonoBehaviour
 {
     Vector2 moveVector;
     Rigidbody rb;
+    Vector3 camForward;
+    Vector3 camRight;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,19 +21,22 @@ public class movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 forward = new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z);
-        Vector3 sideWays = new Vector3(Camera.main.transform.right.x, 0f, Camera.main.transform.right.z);
-
-        rb.AddForce(forward.normalized * moveVector.y + sideWays.normalized * moveVector.x, ForceMode.VelocityChange);
-
         if (moveVector != Vector2.zero)
         {
+            rb.AddForce(camForward.normalized * moveVector.y + camRight.normalized * moveVector.x, ForceMode.VelocityChange);
             rb.rotation = Quaternion.LookRotation(rb.linearVelocity.normalized);
         }
     }
 
     public void OnMove(InputValue value)
     {
+        //* why do we store cam forward and right? 
+        //* because when a camera moves between positions, as long as the player keeps holding the
+        //* same movement key/direction, we want to keep moving in that direction 
+        //* making it feel better than instantly changing direction while holding the same key
+        //* when switching between cam positions
         moveVector = value.Get<Vector2>();
+        camForward = new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z);
+        camRight = new Vector3(Camera.main.transform.right.x, 0f, Camera.main.transform.right.z);
     }
 }
